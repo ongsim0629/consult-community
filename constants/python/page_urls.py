1495243d78e39865ebc6e563d4da999b1a618d2c
ddubbu <hello.ddubbu@gmail.com>
2024-08-07 17:24:50 +0900

    fix: 게시글 수정 가능 #49

--- constants/python/page_urls.py
+++ constants/python/page_urls.py
@@ -4,7 +4,7 @@ PAGE_URLS = {
     "SIGN_IN": "/signin",
     "SIGN_UP": "/signup",
     "CONCERN_ADD": "/concern/add",
-    "CONCERN_EDIT": "/concern/edit",
+    "CONCERN_EDIT": "/concern/update",
     "CONCERN_DETAIL": "/concern/detail",
     "MYPAGE": "/mypage",
 }
