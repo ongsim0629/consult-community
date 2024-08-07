c3ede6a571e5f21e5f0397adf994f0898a23dff6
s1_1v <s1_1v@naver.com>
2024-08-07 13:44:37 +0900

    feat: 고민 수정 및 삭제 API 추가 #34

--- apps/concern_update.py
+++ apps/concern_update.py
@@ -24,16 +24,39 @@ def strToBool(s):
 ## updateConcern 렌더링
 @concern_update_bp.route("/concern/update", methods=["GET"])
 def updateConcernForm():
-    return 1
+    # 먼저 고민 아이디 받아와서
+    concernId = request.args.get("concern_id")
+    concern = db.concerns.find_one({"_id": ObjectId(concernId)})
+    
+    return render_template("editConcern.html", concern=concern)
 
 
 ## Concern 업데이트
 @concern_update_bp.route("/concern/update", methods=["POST"])
 def updateConcern():
-    return 1
+    data = request.get_json()
+    new_title = data.get('new_title')
+    new_content = data.get('new_content')
+    concernId = data.get('concern_id')
+    revealed = data.get('revealed')
+    
+    if not all([new_title, new_content, concernId, revealed]):
+        return jsonify({"message": "모든 필드를 입력해주세요."}), 400
+    
+    db.concerns.update_one({"_id": ObjectId(concernId)}, {'$set': {"title": new_title, "content": new_content, "revealed": strToBool(revealed)}})
+    
+    return jsonify({"message": "업데이트되었습니다."}), 200
+
 
 
 ## Concern 삭제 (API)
 @concern_update_bp.route("/concern/delete", methods=["DELETE"])
 def deleteConcern():
-    return 1
+    data = request.get_json()
+    concernId = data.get('concern_id')
+    
+    if concernId:
+        db.concerns.delete_one({"_id": ObjectId(concernId)})
+        return jsonify({"message": "삭제되었습니다."}), 200
+    else:
+        return jsonify({"message": "삭제에 실패했습니다."}), 400
