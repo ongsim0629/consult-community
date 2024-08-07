776e7299be6d1578d18f6404da55e688ae4f9c0b
ddubbu <hello.ddubbu@gmail.com>
2024-08-07 09:11:37 +0900

    feat: 페이지 상수 적용 #23

--- apps/rendering_test.py
+++ apps/rendering_test.py
@@ -1,39 +1,40 @@
 import os
 from flask import Blueprint, render_template
+from constants.python.page_urls import PAGE_URLS
 
 rendering_bp = Blueprint("rendering_bp", __name__)
 
 
-@rendering_bp.route("/signup")
+@rendering_bp.route(PAGE_URLS["SIGN_UP"])
 def signup():
     return render_template("signUp.html")
 
 
-@rendering_bp.route("/signin")
+@rendering_bp.route(PAGE_URLS["SIGN_IN"])
 def signin():
     return render_template("signIn.html")
 
 
-@rendering_bp.route("/concerns")
+@rendering_bp.route(PAGE_URLS["CONCERN_LIST"])
 def concerns():
     return render_template("concernList.html")
 
 
-@rendering_bp.route("/addconcern")
+@rendering_bp.route(PAGE_URLS["CONCERN_ADD"])
 def addconcern():
     return render_template("addConcern.html")
 
 
-@rendering_bp.route("/editconcern")
+@rendering_bp.route(PAGE_URLS["CONCERN_EDIT"])
 def editconcern():
     return render_template("editConcern.html")
 
 
-@rendering_bp.route("/concerndetail")
+@rendering_bp.route(PAGE_URLS["CONCERN_DETAIL"])
 def concerndetail():
     return render_template("concernDetail.html")
 
 
-@rendering_bp.route("/mypage")
+@rendering_bp.route(PAGE_URLS["MYPAGE"])
 def mypage():
     return render_template("myPage.html")
