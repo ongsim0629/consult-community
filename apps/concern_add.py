6a9bfe1057a9e410794d9f2c9c28ea343c86fabd
Hyobeom Jang <gyqjajang@gmail.com>
2024-08-07 10:28:13 +0900

    refactor:concern.py 기능별 분리 #30

---
+++ apps/concern_add.py
@@ -0,0 +1,50 @@
+import os
+from flask import Blueprint, jsonify, render_template, request, redirect
+from dotenv import load_dotenv
+from bson import ObjectId
+from pymongo import MongoClient
+from datetime import datetime
+
+load_dotenv()
+MONGO_DB_URI = os.environ.get("MONGO_DB_URI")
+MONGO_DB_NAME = os.environ.get("MONGO_DB_NAME")
+
+concern_add_bp = Blueprint("concern_add_bp", __name__)
+
+client = MongoClient(MONGO_DB_URI)
+db = client[MONGO_DB_NAME]
+
+now = datetime.now()
+
+
+def strToBool(s):
+    return s == "true"
+
+
+## addConcern 화면 렌더링
+@concern_add_bp.route("/concern/add", methods=["GET"])
+def addConcernForm():
+
+    return render_template("addConcern.html")
+
+
+## addConcern에서 고민 추가
+@concern_add_bp.route("/concern/add", methods=["POST"])
+def addConcern():
+    formData = request.form
+    concernData = {
+        "title": formData["title"],
+        "content": formData["content"],
+        "revealed": strToBool(formData["revealed"]),
+        "view_count": 0,
+        "created_at": now,
+        "created_by": "닉네임TEST1",  # 토큰에서 값 가져온다.
+        "updated_at": now,
+    }
+    print(concernData)
+
+    insertData = db.concerns.insert_one(concernData)
+    print(concernData)
+    return redirect("/concern/concernList")
+    ## TODO : 위에꺼 없애고 만들어진 고민으로 리다이렉트
+    ## : JWT에서 닉네임 받기
