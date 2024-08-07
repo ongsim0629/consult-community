59ae64942d8c279270cd9306b090a2887895b0f7
ddubbu <hello.ddubbu@gmail.com>
2024-08-07 09:11:13 +0900

    chore: 페이지 상수 분리 #23

---
+++ static/js/page-urls.js
@@ -0,0 +1,14 @@
+// fyi. 페이지 이동이 필요할 시 해당 상수를 이용해주세요
+
+const PAGE_URLS = {
+    SIGN_IN: '/',
+    SIGN_UP: '/signup',
+    CONCERN_LIST: '/concerns',
+    CONCERN_ADD: '/addconcern',
+    CONCERN_EDIT: '/editconcern',
+    CONCERN_DETAIL: '/concerndetail',
+    MYPAGE: '/mypage',
+
+    // 기타
+    GITHUB_REPO: 'https://github.com/IN6P/consult-community',
+};
